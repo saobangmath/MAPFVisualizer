8912a70ee11f05f584da0431dfef70527443abb6
Yong Xin <yongxin.ang@gigworks.co>
2021-10-12 15:16:47 +0800

    Creation of the react app base

---
+++ ap_visualizer/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./images/ap_visualizer-logo.png";
+import Landing_page from "./components/layouts/Landing_page";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      {/* <Landing_page /> */}
+      {/* <header className="App-header">
+       
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ ap_visualizer/src/components/layouts/Landing_page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing_page = () => {
+    return (
+        <div>
+            <h1>Fk off</h1>
+        </div>
+    )
+}
+
+export default Landing_page
