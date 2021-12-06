0f1559e72ffdfbbcb1c170e08f6f65be68d0beef
Saobangmath <anhtai2672000@gmail.com>
2021-12-07 01:09:47 +0800

    add Map.js

--- pathFinding/cbs/Map.js
+++ pathFinding/cbs/Map.js
@@ -2,33 +2,56 @@
  * map data structure
  */
 
+
 const fs = require('fs')
 
 class Map{
-    constructor(height, width, agents) {
-        this.height = height
-        this.width = width
-        this.agents = agents
-        this.grid = []
+    constructor() {
+        this.height = 0 // height of the grid map
+        this.width = 0 // width of the grid map
+        this.agents = {} // {agent_id : {START, DEST}}
+        this.no_agents = 0; // total number of agents
+        this.grid = [] // the grid map
     }
 
-    // TODO: thinking about what should be stored inside the kiva.map configuration
-    readMap(filename){ // read the map from some configuration
-        let info = ""
+    readMap(filename){ // read the map from some configuration file
         try{
-            info = fs.readFileSync(filename,  {encoding:'utf8', flag:'r'})
+            let raw_info = fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'})
+            let infos = raw_info.split("\r\n")
+            this.height = parseInt(infos[0])
+            this.width = parseInt(infos[1])
+            for (let row = 0; row < this.height; row++){
+                this.grid.push(infos[2 + row])
+            }
+            this.no_agents = parseInt(infos[2 + this.height])
+            for (let id = 1; id <= this.no_agents; id++){
+                let [x_start, y_start, x_dest, y_dest] = infos[2 + this.height + id].split(",")
+                this.agents[id] = {"START" : [parseInt(x_start), parseInt(y_start)],
+                                   "DEST" : [parseInt(x_dest), parseInt(y_dest)]}
+            }
         }catch (err){
             console.log(`Read map err: ${err}`)
         }
-        return info
     }
 
     get getHeight(){  // get height of the grid
-        return this.grid.length;
+        return this.height;
     }
 
     get getWidth(){ // get width of the grid
-        return this.grid[0].length;
+        return this.width;
+    }
+
+    get getNoAgents(){ // get total number of agents;
+        return this.no_agents;
+    }
+
+    get getAgents(){ // get all agents locations;
+        return this.agents;
+    }
+
+    get getGrid(){ // get the grid map
+        return this.grid;
     }
 }
 
