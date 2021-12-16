bdd8a46dcc34f613029e63bcb8b6a6a8b7124464
theturtle27 <62739849+theturtle27@users.noreply.github.com>
2021-12-16 17:41:24 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/saobangmath/MAPFVisualizer""
    This reverts commit daf29e42a49857f00ee08a04ed326b65bc54ffc2.

---
+++ ap_visualizer/src/components/layouts/Agents/Agent_Table.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Agent_Table = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Agent_Table;
