507e0bad8230f5784bd57e9b0d1a73c28528d832
theturtle27 <62739849+theturtle27@users.noreply.github.com>
2021-12-23 03:06:16 +0800

    Added change map button
    Added change map button to cater for different preset map layouts we have in the future

--- ap_visualizer/src/App.js
+++ ap_visualizer/src/App.js
@@ -10,14 +10,53 @@ import AgentsPage from "./components/layouts/Agents/Agents_Page";
 import { useState } from "react";
 
 // hard coded map for now;
-let grid = [['@', '.' , '.', '@'],
-            ['.', '.', '.', '.'],
-            ['.', '.', '.', '.'],
-            ['@', '.', '.', '@']]
+let grid = [['@', '1' , '.', '@'],
+                  ['.', '.', '.', '.'],
+                  ['.', '.', '.', '.'],
+                  ['@', '.', '.', '@']];
+
 
 function App() {
   var [agents, setAgentList] = useState([]);
   var [gridMap, setGridMap] = useState(grid);
+  var [mapNumber, setMapNumber] = useState(1);
+
+  function changeGrid(){
+    if (mapNumber != 3){
+      setMapNumber(prevMapnumber => prevMapnumber + 1)}
+    if (mapNumber === 3){
+      setMapNumber(prevMapnumber => 1)}
+
+    switch(mapNumber) {
+      case 1:
+        setGridMap(prevGridMap => 
+        [['@', '2' , '.','@'],
+        ['.', '.', '.', '.'],
+        ['.', '.', '.', '.'],
+        ['@', '.', '.', '@']])
+        break;
+      case 2:
+        setGridMap(prevGridMap => 
+        [['@', '3' , '.','@'],
+        ['.', '.', '.', '.'],
+        ['.', '.', '.', '.'],
+        ['@', '.', '.', '@']])
+        break;
+      case 3:
+        setGridMap(prevGridMap => 
+        [['@', '1' , '.','@'],
+        ['.', '.', '.', '.'],
+        ['.', '.', '.', '.'],
+        ['@', '.', '.', '@']])
+        break;
+      default:
+        setGridMap(prevGridMap => 
+        [['@', '.' , '.','@'],
+        ['.', '.', '.', '.'],
+        ['.', '.', '.', '.'],
+        ['@', '.', '.', '@']])
+  }
+}
 
   const getAgentList = (agent) => {
     setAgentList(agent);
@@ -44,6 +83,8 @@ function App() {
           ></AgentsPage>
         </div>
       </div>
+      <button onClick={changeGrid}>Change Map</button>
+      <span>{mapNumber}</span>
     </div>
   );
 }
