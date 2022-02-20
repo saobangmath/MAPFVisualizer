26524b4e27d811de03cfd5318da1bfd81ae92350
Yong Xin <yongxin.ang@gigworks.co>
2022-02-20 21:31:00 +0800

    Adding a mini robot helper into the web

--- ap_visualizer/src/App.js
+++ ap_visualizer/src/App.js
@@ -229,6 +229,20 @@ function App() {
             setEndBoard={setEndBoard}
           ></AgentsPage>
         </div>
+        <div class="cute-robot-v1">
+          <div class="circle-bg">
+            <div class="robot-ear left"></div>
+            <div class="robot-head">
+              <div class="robot-face">
+                <div class="eyes left"></div>
+                <div class="eyes right"></div>
+                <div class="mouth"></div>
+              </div>
+            </div>
+            <div class="robot-ear right"></div>
+            <div class="robot-body"></div>
+          </div>
+        </div>
       </div>
       {mapModal && (
         <div className="modalAdd">
