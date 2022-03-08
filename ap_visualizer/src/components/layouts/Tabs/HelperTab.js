e7bf7a007bc9d659cd16b448283e7e7718c7b040
Yong Xin <yongxin.ang@gigworks.co>
2022-03-08 18:41:13 +0800

    update on limits,helpertab and landingpage

--- ap_visualizer/src/components/layouts/Tabs/HelperTab.js
+++ ap_visualizer/src/components/layouts/Tabs/HelperTab.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Tabs } from "react-simple-tabs-component";
 // (Optional) if you don't want to include bootstrap css stylesheet
 import "react-simple-tabs-component/dist/index.css";
+import classes from "../Tabs/HelperTab.module.css";
 // Component Example
 const TabOne = () => {
   return (
     <>
-      <h4>Who We Are</h4>
-      <p>We are creator of AP Visualizer.</p>
+      <h4>What is AP Visualizer?</h4>
+      <p>
+        AP Visualizer is a robot fleet management application that simulate
+        robots moving in a grid map. It will use the most optimised algorithm to
+        find the fastest path to the specific destination.
+      </p>
     </>
   );
 };
@@ -15,32 +20,64 @@ const TabOne = () => {
 const TabTwo = () => {
   return (
     <>
-      <h3>How To Start</h3>
-      <p>The steps is actually easy.</p>
-      <p>
-        Type of status: -Available (When there's no task being assigned to the
-        agents) -Assigned (When the agent is being assigned with a task) -Busy
-        (the agent is running the algorithm) -Completed (Agent has finish
-        running the algorithm and is available for user to view their algorithm
-        details)
-      </p>
-      <p>
-        -Type of actions: -Assign (the agent is available for user to assign the
-        robot task) (CLICKABLE) -Ready (After agent has been assigned with an
-        task) (CLICKABLE) -Running (Agent is still running the algorithm)(NOT
-        CLICKABLE) -Detail (the agent has finish running the algorithm and users
-        is able to click on it to view the details) (CLICKABLE)
-      </p>
+      <h3>AP Visualizer Common-Terminology</h3>
+      <h5>Type of status</h5>
+      <ul className={classes.list_style}>
+        <li>Available: The robot is available for assigning of task.</li>
+        <li>Busy: The robot is still running the algorithm.</li>
+        <li>Completed: The robot has finished running the algorithm.</li>
+      </ul>
+      <h5>Type of actions</h5>
+      <ul className={classes.list_style}>
+        <li>Assign: You can click on it to assign task.</li>
+        <li>
+          Ready: The robot is ready to start, but you can still make changes to
+          the destination.
+        </li>
+        <li>
+          Running: The robot has finished running the algorithm.Nothing can be
+          change at this point of time.
+        </li>
+        <li>
+          Detail: The robot has finished running the algorithm, you can click on
+          it to view the details.
+        </li>
+      </ul>
     </>
   );
 };
 const TabThree = () => {
   return (
     <>
-      <h3>Tab One</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
-        sint illum iusto nostrum cumque qui voluptas tenetur inventore ut quis?
+      <h3>Frequent Asked Questions</h3>
+      <p className={classes.section_style}>
+        Q1. What is the max number of robots I can add into my map?
+      </p>
+      <p className={classes.answer_style1}>
+        Ans: Below are the max number of robots that you can add into our map:
+      </p>
+      <ul className={classes.list_style}>
+        <li>-Less than 30 squares: 2 robots</li>
+        <li>-More than 30 squares but less than 100 squares: 5 robots</li>
+        <li>-More than 100 squares: 8 robots</li>
+      </ul>
+      <p className={classes.section_style}>
+        Q2. How do I upload/change my map?
+      </p>
+      <p className={classes.answer_style}>
+        Ans: You can change the map by clicking "Default Map" at the bottom of
+        our page.
+      </p>
+      <p className={classes.section_style}>
+        Q3. What is the recommended speed and algorithms to start with?
+      </p>
+      <p className={classes.answer_style}>
+        Ans: Our recommended speed is "Average" and algorithm will be "CBS".
+      </p>
+      <p className={classes.section_style}>Q4. What is the chart showing?</p>
+      <p className={classes.answer_style}>
+        Ans: Execution Time Comparison Chart refers to the time needed to finish
+        running the algorithms. The shorter the time the better the algorithm.
       </p>
     </>
   );
@@ -52,16 +89,22 @@ const tabs = [
     Component: TabOne, // Tab Body - JSX.Element
   },
   {
-    label: "How To Start",
+    label: "Common-Terminology",
     Component: TabTwo,
   },
   {
-    label: "Map guidelines",
+    label: "Frequent Asked Questions",
     Component: TabThree,
   },
 ];
 const HelperTab = () => {
-  return <Tabs tabs={tabs} /* Props */ />;
+  return (
+    <Tabs
+      textColor="secondary"
+      indicatorColor="secondary"
+      tabs={tabs} /* Props */
+    />
+  );
 };
 
 export default HelperTab;
